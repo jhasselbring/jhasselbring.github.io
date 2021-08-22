f32ea229aee83dfc06fa64288658d2de68845ff7
Jan Hasselbring <janmykhail.hasselbring@outlook.com>
2021-08-21 19:51:02 -0500

    sync

--- components/Code.vue
+++ components/Code.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="bg-gray-500">
-    Code
+    <pre>
+      const Jan = () => {
+        return {
+            fullName: "Jan Mykhail Hasselbring",
+            nickName: "Jay",
+            email: "janmykhail.hasselbring@outlook.com",
+            phone: "224-637-5331"
+        }
+      }
+    </pre>
   </div>
 </template>
 
